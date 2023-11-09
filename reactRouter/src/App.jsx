ec4f2e51661b6bd77237dde7bfb5c86917c9d6be
Nicole Park <nicolepark@bellsouth.net>
2023-11-09 14:41:03 -0500

    added components red, blue, home

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,20 +1,31 @@
-import { useState } from 'react'
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from "./components/Blue";
+import Red from "./components/Red";
+import Home from "./components/Home";
 
-import './App.css'
+import "./App.css";
 
 function App() {
- 
+    return (
+        <>
+            <div id="container">
+                {/* <h1>React router</h1> */}
 
-  return (
-    <>
-     <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
-    </div>
-    
-    </>
-  )
+                <div id="navbar">
+                    <Link to="/">Home</Link>
+                    <Link to="/blue">Blue</Link>
+                    <Link to="/red">Red</Link>
+                </div>
+                <div id="main-section">
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route path="/blue" element={<Blue />}></Route>
+                        <Route path="/red" element={<Red />}></Route>
+                    </Routes>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ reactRouter/src/components/Red.jsx
@@ -0,0 +1,11 @@
+function Red() {
+    return (
+        <>
+            <div className="red">
+                <h1>RED</h1>
+            </div>
+        </>
+    );
+}
+
+export default Red;
